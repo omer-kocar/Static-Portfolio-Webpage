bcbbd9a4aba0f5d16f62b13fed12e7a3dba8d0bf
Ömer Koçar <kocaromer@gmail.com>
2022-02-24 01:45:12 +0300

    main files add

---
+++ app.js
@@ -0,0 +1,22 @@
+// choosing container for appendChild
+// const container = document.querySelector(".container");
+
+// // My array for loop
+// let elements = ["home", "about", "my work", "contanct"];
+
+// // Creating ul and li
+// let ul = document.createElement("ul");
+// container.appendChild(ul);
+// ul.setAttribute("id", "list");
+
+// elements.forEach((element) => {
+//   let li = document.createElement("li");
+//   ul.appendChild(li);
+//   let a = document.createElement("a");
+//   li.appendChild(a);
+//   a.setAttribute("href", "#");
+//   a.innerHTML = element;
+
+// });
+
+// let list = document.getElementById("list");
